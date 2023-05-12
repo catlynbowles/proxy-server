6b647e3a721d14d6905d6b79563df53426f664fb
Catlyn Bowles <bowlescatlyn@gmail.com>
2023-05-11 18:19:00 -0600

    add vercel.json

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-import express from 'express'
+import express from "express";
 import fetch from "node-fetch";
-import cors from 'cors'
+import cors from "cors";
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
-app.get('/', async (req, res) => {
-  if (req.get('feeling')) {
-    req._custom = req.get('feeling')
+app.get("/", async (req, res) => {
+  if (req.get("feeling")) {
+    req._custom = req.get("feeling");
   }
   const response = await fetch(
     `http://api.wefeelfine.org:8080/ShowFeelings?display=xml&returnfields=sentence,imageid,postdate&feeling=${req._custom}&limit=500&extraimages=8`
   );
   const body = await response.text();
-  res.json(body)
-})
+  res.json(body);
+});
 
 app.listen(3001, () => {
-  console.log('Listening on port 3001!')
-})
+  console.log("Listening on port 3001!");
+});
+
+// Export the Express API
+module.exports = app;
