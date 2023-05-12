b0817bb13f5f6ad77a262bd81ac860cccd8b9341
Catlyn Bowles <bowlescatlyn@gmail.com>
2023-05-11 18:58:55 -0600

    update

--- server.js
+++ server.js
@@ -7,13 +7,6 @@ app.use(
   cors()
 );
 
-app.options("/", function (req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "*");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  res.end();
-});
-
 app.get('/', async (req, res) => {
   // if (req.get('feeling')) {
   //   req._custom = req.get('feeling')
@@ -32,4 +25,4 @@ app.listen(3001, () => {
 });
 
 // Export the Express API
-export default app;
+export default app
